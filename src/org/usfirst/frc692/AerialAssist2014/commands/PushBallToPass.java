e0137dff5c5b273fe41a53fbaf772eedc6b6e6d9
Fembot <Fembot@Web>
2014-02-08 17:20:22 -0800

    updated PushBallToPass

--- src/org/usfirst/frc692/AerialAssist2014/commands/PushBallToPass.java
+++ src/org/usfirst/frc692/AerialAssist2014/commands/PushBallToPass.java
@@ -36,7 +36,7 @@ public class  PushBallToPass extends Command {
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
     // Called once after isFinished returns true
     protected void end() {
